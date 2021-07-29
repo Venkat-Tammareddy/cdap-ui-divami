ce19e074167641406fa2da43af2d80f50b95a007
tharunpola <tharunpola007@gmail.com>
2021-07-29 18:13:50 +0530

    integrated tags api to create ingest flow

--- app/cdap/components/Ingestion/ingestionContext.tsx
+++ app/cdap/components/Ingestion/ingestionContext.tsx
@@ -3,7 +3,6 @@ import React, { useState, useContext, useCallback, createContext } from 'react';
 export const ingestionContext = createContext({ draftObj: {}, setDraftObjfn: (item: any) => {} });
 
 export const AppProvider = ({ children }) => {
-  // const appContext = useContext(ingestionContext);
   const [draftObj, setDraftObj] = useState({ ki: 'li' });
   const setDraftObjfn = (item) => {
     setDraftObj(item);
